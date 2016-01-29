9c500f23bf4b76fe691698611642cd68025648a1
chile12 <markus.freudenberg@gmail.com>
2016-01-29 13:37:43 +0100

    added ns folder

--- ns/deref.sh
+++ ns/deref.sh
@@ -3,7 +3,7 @@
 rapper -i turtle -o rdfxml-abbrev core.ttl > core.rdf
 
 # OWL/XML
-wget "http://mowl-power.cs.man.ac.uk:8080/converter/convert?ontology=http://dataid.dbpedia/ns/core.ttl&format=OWL/XML"  -O core.owl
+wget "http://mowl-power.cs.man.ac.uk:8080/converter/convert?ontology=http://rdfunit.aksw.org/ns/core.ttl&format=OWL/XML"  -O core.owl
 
 # HTML Display
-wget http://www.essepuntato.it/lode/http://dataid.dbpedia/ns/core.rdf -O core.html
+wget http://www.essepuntato.it/lode/http://dataid.dbpedia.org/ns/core.rdf -O core.html
